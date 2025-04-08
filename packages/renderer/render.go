0583cdcd309b23a942400e1d40b39d53e0ceb83c
Jason Stoddard <jason.stoddard@crowdstrike.com>
2025-04-08 11:33:40 -0400

    added renderer in gRPC server

--- packages/renderer/render.go
+++ packages/renderer/render.go
@@ -1,37 +1,47 @@
 package renderer
 
 import (
+	"bytes"
 	"context"
 	"errors"
-	v8 "rogchap.com/v8go"
-	"sync"
+	"html/template"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 type Server struct{}
 
 func (s *Server) RenderPage(ctx context.Context, message *ReqMessage) (*ResMessage, error) {
-	// setup v8 runtime
-	node := NewNodeCtx()
-
 	meta := message.Metadata
 	if meta == nil {
 		return nil, errors.New("no metadata")
 	}
 
+	tmplName := "shell"
+	tmpl, err := RenderTemplate(tmplName)
+	if err != nil {
+		return nil, err
+	}
+	buf := bytes.NewBuffer(nil)
+	if err := tmpl.Execute(buf, meta); err != nil {
+		log.Printf("err: %v", err)
+		return nil, err
+	}
+
 	return &ResMessage{
-		Data: "",
+		Markup: buf.String(),
 	}, nil
 }
 
-type NodeCtx *v8.Context
+func RenderTemplate(tmplName string) (*template.Template, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
 
-var once sync.Once
-var nodeInstance NodeCtx
+	pathToTemplate := filepath.Join(dir, "templates", tmplName+".html")
+	tmpl, err := template.ParseFiles(pathToTemplate)
 
-func NewNodeCtx() NodeCtx {
-	once.Do(func() {
-		nodeInstance = v8.NewContext()
-	})
-
-	return nodeInstance
+	return tmpl, nil
 }
