3964a32d79be05bcf42bc4989ee341d0b5f11dbf
Jason Stoddard <jason.stoddard@crowdstrike.com>
2025-07-29 09:17:18 -0400

    added integration test for helper

--- ember-ssr/tests/integration/helpers/format-timestamp-test.js
+++ ember-ssr/tests/integration/helpers/format-timestamp-test.js
@@ -3,15 +3,15 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Helper | format-timestamp', function(hooks) {
+module('Integration | Helper | format-timestamp', function (hooks) {
   setupRenderingTest(hooks);
 
   // Replace this with your real tests.
-  test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+  test('it renders', async function (assert) {
+    this.set('d', '07-29-2025');
 
-    await render(hbs`{{format-timestamp inputValue}}`);
+    await render(hbs`{{format-timestamp this.d}}`);
 
-    assert.equal(this.element.textContent.trim(), '1234');
+    assert.equal(this.element.textContent.trim(), 'Jul 29, 2025 00:00.00 AM');
   });
 });
