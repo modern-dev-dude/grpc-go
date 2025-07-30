ed01a8e418dfb8268819126ba98b291e37f99336
Jason Stoddard <jason.stoddard@crowdstrike.com>
2025-07-30 11:46:05 -0400

    updated tests

--- ember-ssr/app/services/auth.js
+++ ember-ssr/app/services/auth.js
@@ -1,6 +1,8 @@
 import Service from '@ember/service';
 import { inject as service  } from '@ember/service';
 import Router from "@ember/routing/router"
+import { action } from "@ember/object"
+
 const AUTH_KEY  ='some-userid'
 
 export default class AuthService extends Service {
@@ -15,6 +17,12 @@ export default class AuthService extends Service {
     loginWithUserId(userId){
         window.localStorage.setItem(AUTH_KEY, userId)
         this.router.transitionTo("teams")
+    }
+
+    @action
+    logout(){
+        window.localStorage.removeItem(AUTH_KEY)
+        this.router.transitionTo("login")
     }
 
 }
--- ember-ssr/tests/acceptance/logout-test.js
+++ ember-ssr/tests/acceptance/logout-test.js
@@ -1,17 +1,22 @@
 import { module, test } from 'qunit';
 import { visit, currentURL, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import MockAuthAervice from '../stubs/auth-service';
 
 module('Acceptance | logging out', function(hooks) {
   setupApplicationTest(hooks);
 
+  hooks.beforeEach(function() {
+    this.owner.register('service:auth', MockAuthAervice)
+  })
+
   test('visiting /teams and clicking logout', async function(assert) {
+    this.owner.lookup('service:auth').currentUserId = '1'
     await visit('/teams');
     assert.equal(currentURL(), '/teams');
 
     await click(".team-sidebar__logout-button")
 
     assert.equal(currentURL(), '/login');
-
   });
 });
