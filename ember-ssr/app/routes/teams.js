f9d3bb0238f2e55a702931dfc0fc8fb6d2fdd259
Jason Stoddard <jason.stoddard@crowdstrike.com>
2025-07-30 12:37:34 -0400

    added dynamic data for channels

--- ember-ssr/app/routes/teams.js
+++ ember-ssr/app/routes/teams.js
@@ -1,27 +1,7 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import AuthService from 'shlack/services/auth';
-
-export const ALL_TEAMS = [
-  {
-    id: 'linkedin',
-    name: 'LinkedIn',
-    order: 2,
-    iconUrl: '/assets/img/linkedin.png',
-  },
-  {
-    id: 'ms',
-    name: 'Microsoft',
-    order: 3,
-    iconUrl: '/assets/img/microsoft.png',
-  },
-  {
-    id: 'avengers',
-    name: 'Avengers',
-    order: 4,
-    iconUrl: '/assets/img/avengers.jpg',
-  },
-];
+import fetch from 'fetch';
 
 export default class TeamsRoute extends Route {
   /**
@@ -37,7 +17,8 @@ export default class TeamsRoute extends Route {
     }
   }
 
-  async model(){ 
-    return Promise.resolve(ALL_TEAMS)
+  async model(){
+    const res = await fetch('/api/teams')
+    return res.json()
   }
 }
